b537b825c056e88630bd6ee638d109a0b0a8cefb
DmitriySh <dashishmakov@gmail.com>
2015-05-28 04:57:35 +0300

    Server: changes into pom, were added examples from git for protobuf.

---
+++ server/src/main/java/ru/shishmakov/server/core/example/WorldClockClient.java
@@ -0,0 +1,67 @@
+/*
+package ru.shishmakov.server.core.example;
+
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.Channel;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.InsecureTrustManagerFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+*/
+/**
+ * Sends a list of continent/city pairs to a {@link WorldClockServer} to
+ * get the local times of the specified cities.
+ *//*
+
+public final class WorldClockClient {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final String HOST = System.getProperty("host", "127.0.0.1");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8463"));
+    static final List<String> CITIES = Arrays.asList(System.getProperty(
+            "cities", "Asia/Seoul,Europe/Berlin,America/Los_Angeles").split(","));
+
+    public static void main(String[] args) throws Exception {
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (SSL) {
+            sslCtx = SslContext.newClientContext(InsecureTrustManagerFactory.INSTANCE);
+        } else {
+            sslCtx = null;
+        }
+
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .handler(new WorldClockClientInitializer(sslCtx));
+
+            // Make a new connection.
+            Channel ch = b.connect(HOST, PORT).sync().channel();
+
+            // Get the handler instance to initiate the request.
+            WorldClockClientHandler handler = ch.pipeline().get(WorldClockClientHandler.class);
+
+            // Request and get the response.
+            List<String> response = handler.getLocalTimes(CITIES);
+
+            // Close the connection.
+            ch.close();
+
+            // Print the response at last but not least.
+            for (int i = 0; i < CITIES.size(); i ++) {
+                System.out.format("%28s: %s%n", CITIES.get(i), response.get(i));
+            }
+        } finally {
+            group.shutdownGracefully();
+        }
+    }
+}
+*/
---
+++ server/src/main/java/ru/shishmakov/server/core/example/WorldClockServer.java
@@ -0,0 +1,50 @@
+/*
+package ru.shishmakov.server.core.example;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+import io.netty.handler.logging.LogLevel;
+import io.netty.handler.logging.LoggingHandler;
+import io.netty.handler.ssl.SslContext;
+import io.netty.handler.ssl.util.SelfSignedCertificate;
+
+*/
+/**
+ * Receives a list of continent/city pairs from a {@link WorldClockClient} to
+ * get the local times of the specified cities.
+ *//*
+
+public final class WorldClockServer {
+
+    static final boolean SSL = System.getProperty("ssl") != null;
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8463"));
+
+    public static void main(String[] args) throws Exception {
+        // Configure SSL.
+        final SslContext sslCtx;
+        if (SSL) {
+            SelfSignedCertificate ssc = new SelfSignedCertificate();
+            sslCtx = SslContext.newServerContext(ssc.certificate(), ssc.privateKey());
+        } else {
+            sslCtx = null;
+        }
+
+        EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+        EventLoopGroup workerGroup = new NioEventLoopGroup();
+        try {
+            ServerBootstrap b = new ServerBootstrap();
+            b.group(bossGroup, workerGroup)
+                    .channel(NioServerSocketChannel.class)
+                    .handler(new LoggingHandler(LogLevel.INFO))
+                    .childHandler(new WorldClockServerInitializer(sslCtx));
+
+            b.bind(PORT).sync().channel().closeFuture().sync();
+        } finally {
+            bossGroup.shutdownGracefully();
+            workerGroup.shutdownGracefully();
+        }
+    }
+}
+*/
